7304027ecdd9854e96ddd272401318b72617a193
Chen Ruofan <physcrf@qq.com>
2019-05-07 11:20:47 +0800

    unfill paragraph

--- setting/utility.setting.el
+++ setting/utility.setting.el
@@ -17,4 +17,15 @@
 (global-set-key (kbd "C-S-O") 'open-previous-line)
 (global-set-key (kbd "<f5>") 'compile-now) 
 
+
+;; https://www.emacswiki.org/emacs/UnfillParagraph
+(defun unfill-paragraph (&optional region)
+  "Takes a multi-line paragraph and makes it into a single line of text."
+  (interactive (progn (barf-if-buffer-read-only) '(t)))
+  (let ((fill-column (point-max))
+	(emacs-lisp-docstring-fill-column t))
+    (fill-paragraph nil region)))
+
+(global-set-key (kbd "M-s") 'unfill-paragraph)
+
 (provide 'utility.setting)
