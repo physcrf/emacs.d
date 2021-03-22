c123d95f91f457f33345cfc0c27f7aa1c13ed3f8
CRF <physcrf>
2021-03-22 16:17:21 +0800

    clean

--- elpa/xr-1.9/xr-pkg.el
+++
@@ -1,2 +0,0 @@
-;; Generated package description from xr.el
-(define-package "xr" "1.9" "Convert string regexp to rx notation" 'nil :url "http://elpa.gnu.org/packages/xr.html" :keywords '("lisp" "maint" "regexps"))
